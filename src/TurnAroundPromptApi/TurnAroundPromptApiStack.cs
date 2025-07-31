14851f0e11b6987f13ba77e3aee333e212bda2e2
sayTG <busarithienkie@gmail.com>
2025-07-31 20:41:53 +0100

    Initial commit

---
+++ src/TurnAroundPromptApi/TurnAroundPromptApiStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace TurnAroundPromptApi
+{
+    public class TurnAroundPromptApiStack : Stack
+    {
+        internal TurnAroundPromptApiStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
